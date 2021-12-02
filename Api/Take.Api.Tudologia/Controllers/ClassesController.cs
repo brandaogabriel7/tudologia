f7340dfa3c6cffc8896708e2b44bd49db7c8aa2c
Gabriel Silva Brandão <gabrielsb@take.net>
2021-12-02 18:09:31 -0300

    docs: add docs to classes controller

--- Api/Take.Api.Tudologia/Controllers/ClassesController.cs
+++ Api/Take.Api.Tudologia/Controllers/ClassesController.cs
@@ -6,17 +6,26 @@ using Take.Api.Tudologia.Facades.Interfaces;
 
 namespace Take.Api.Tudologia.Controllers
 {
+    /// <summary>
+    /// Handles actions related to Tudologia classes.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ClassesController : ControllerBase
     {
         private readonly IClassesFacade _classesFacade;
 
+        /// <summary>
+        /// Injects IClassesFacade.
+        /// </summary>
         public ClassesController(IClassesFacade classesFacade)
         {
             _classesFacade = classesFacade;
         }
 
+        /// <summary>
+        /// Gets a menu containing the classes that are still available.
+        /// </summary>
         [HttpGet("availability/menu")]
         public async Task<IActionResult> GetClassesAvailabilityMenuAsync()
         {
