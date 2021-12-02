7853b922db88fb97122d1f38e6c02b4bf8091c6a
Gabriel Silva Brandão <gabrielsb@take.net>
2021-12-02 17:48:12 -0300

    feat: implement logic to mount availability menu

--- Api/Take.Api.Tudologia.Facades/ClassesFacade.cs
+++ Api/Take.Api.Tudologia.Facades/ClassesFacade.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using System.Linq;
 using System.Threading.Tasks;
 
 using Lime.Messaging.Contents;
+using Lime.Protocol;
+
+using Newtonsoft.Json.Linq;
 
 using Take.Api.Tudologia.Facades.Interfaces;
+using Take.Api.Tudologia.Models;
 using Take.Api.Tudologia.Models.UI;
 
 namespace Take.Api.Tudologia.Facades
@@ -22,8 +26,21 @@ namespace Take.Api.Tudologia.Facades
         /// <inheritdoc/>
         public async Task<Select> GetClassesAvailabilityMenuAsync()
         {
-            // TODO: Implementar lógica de criação do menu de disponibilidade.
-            throw new NotImplementedException();
+            var classesResource = await _blipFacade.GetResourceAsync<JsonDocument>(_apiSettings.ClassesResourceName);
+
+            var availableClasses = classesResource.Where(cr =>
+            {
+                var classInformation = (cr.Value as JObject).ToObject<TudologiaClass>();
+                return classInformation.Attendees.Count() < classInformation.MaxCapacity;
+            }).Select(cr => cr.Key);
+
+            var menuOptions = availableClasses.Select(availableClass => new SelectOption { Text = $"Turma {availableClass}" });
+
+            return new Select
+            {
+                Text = _apiSettings.AvailabilityMenuText,
+                Options = menuOptions.ToArray()
+            };
         }
     }
 }
